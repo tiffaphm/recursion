20c5e40a0efd94e5e00c0ef9038a6057bda81e1b
Tiffany Pham <tiffanynpham@gmail.com>
2017-08-09 12:43:56 -0700

    Completed basic requirements.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,33 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+
+  // store class names in an array
+  var classNames = [];
+
+  // create a search that sees if the list of classes has the specified classname
+  function nodeSearch(element) {
+    //make sure that class list is not empty && contains className
+    if (element.classList !== undefined && element.classList.contains(className)) {
+      //if it does, add it to the storage array
+      classNames.push(element);
+    };
+
+    // now that we have a list of classNames
+    // look for only the child nodes of the given element if they exist
+    if (element.hasChildNodes()) {
+      // create a variable to store childNodes
+      var childElements = element.childNodes;
+      // iterate through childNodes and return those of the given element
+      for (var i=0; i < childElements.length; i++) {
+        nodeSearch(childElements[i]);
+      }
+    };
+  };
+
+  // search the element/document
+  nodeSearch(document.body);
+  // return array of class names of elements
+  return classNames;
+
 };
